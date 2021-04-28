38f56103ad9851f63696e1d8e68b521e4e1147ed
Frederique Retsema <fretsema@fretsema.nl>
2021-04-28 16:35:03 +0200

    Minor changes in comments

--- Using Managed Identity with Azure Automation Accounts/Stop-VM1.ps1
+++ Using Managed Identity with Azure Automation Accounts/Stop-VM1.ps1
@@ -1,6 +1,6 @@
 # Stop vm1
 # --------
-# This PowerShell script supports the blog "Using Manaqged Identity with Azure Automation" on the 
+# This PowerShell script supports the blog "Using a Managed Identity with Azure Automation Accounts" on the 
 # AMIS Technology Blog (https://technology.amis.nl)
 
 Connect-AzAccount -Identity
